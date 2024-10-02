69d9200f55b66bc97b80613145fcf90e2b090704
Babina081 <babitamoo333@gmail.com>
2024-10-02 21:19:18 +0545

    add welcome screen

---
+++ components/WelcomeScreen.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { ScrollView, Text, View } from "react-native";
+
+export default function WelcomeScreen() {
+  return (
+    <ScrollView
+      horizontal={false}
+      indicatorStyle={"white"}
+      style={{ flex: "1", padding: 40, backgroundColor: "green" }}
+    >
+      <Text
+        style={{
+          fontSize: 40,
+          color: "black",
+          textAlign: "center",
+          padding: 40,
+          fontWeight: "bold",
+        }}
+      >
+        Welcome To Little Lemon
+      </Text>
+      <Text
+        style={{
+          padding: 40,
+          fontSize: 40,
+          color: "black",
+          textAlign: "center",
+          marginVertical: 8,
+        }}
+      >
+        Little Lemon is a charming neighborhood bistro that serves simple food
+        and classic cocktails in a lively but casual environment. We would love
+        to hear more about your experience with us!
+      </Text>
+    </ScrollView>
+  );
+}
