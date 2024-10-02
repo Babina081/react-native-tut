3d77002e844a98c8ad21f7e6d3f23093f2b5c9df
Babina081 <babitamoo333@gmail.com>
2024-10-02 21:15:51 +0545

    update root page

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import LittleLemonHeader from "./components/LittleLemonHeader";
+import LittleLemonFooter from "./components/LittleLemonFooter";
+import WelcomeScreen from "./components/WelcomeScreen";
+import MenuItems from "./components/MenuItems";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        <LittleLemonHeader />
+        {/* <WelcomeScreen /> */}
+        <MenuItems />
+      </View>
+      <View style={{ backgroundColor: "#495E57" }}>
+        <LittleLemonFooter />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#495E57",
+    fontStyle: "italic",
+    textAlign: "center",
   },
 });
